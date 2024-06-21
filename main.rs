6a6fae2f8e20caa7a68e5f9d45f8ae634d932833
Tony Wakuu <wakuu@muvor-G4uBvE1oZ6sY.api.muvor.com>
2024-06-21 09:28:37 -0400

    MVRP-Rust v1.1

--- main.rs
+++ main.rs
@@ -1,11 +1,11 @@
-use mvrp::server::run_server;
-use mvrp::video_processor::process_segments;
+use mvrp::MVRPServer::new;
+use mvvp::process_segments;
 
 #[tokio::main]
 async fn main() {
     tokio::spawn(async {
         // Start the server
-        run_server("127.0.0.1:8443", "path/to/server-key.pem", "path/to/server-cert.pem").await;
+        new("127.0.0.1:8443", "path/to/server-key.pem", "path/to/server-cert.pem").await;
     });
 
     // Periodically process video segments
---
+++ target/package/mvrp-1.1.0/target/debug/deps/mvrp-b2302322b317e466.d
@@ -0,0 +1,5 @@
+/home/wakuu/Documents/Muvor1/server1.12/mvrp/Rust/target/package/mvrp-1.1.0/target/debug/deps/mvrp-b2302322b317e466: main.rs
+
+/home/wakuu/Documents/Muvor1/server1.12/mvrp/Rust/target/package/mvrp-1.1.0/target/debug/deps/mvrp-b2302322b317e466.d: main.rs
+
+main.rs:
